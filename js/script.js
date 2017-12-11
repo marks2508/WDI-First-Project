a37190cf83000e06811870e68b0347e61ed6377b
Mark S <markscicluna@hotmail.com>
2017-12-11 12:52:31 +0000

    fixed bet display

--- js/script.js
+++ js/script.js
@@ -1,18 +1,76 @@
 console.log('JS loaded - fine');
 $(() => {
-  const $choice = $('.choice');
+
+  const $horse = $('.choice');
+  let $odds = $('.odds');
   const $betting = $('.bet');
+  const $playerWallet = $('.playerWallet');
+  const $onePound = $('.onePound');
+  const $fivePound = $('.fivePound');
+  const $tenPound = $('.tenPound');
+  const $twentyPound = $('.twentyPound');
+  const $fiftyPound = $('.fiftyPound');
+  const $hundredPound = $('.hundredPound');
+  const $reset = $('.resetBet');
+  let $cashAvailable = $('.cashAvailable');
+  const $placeBet = $('.placeBet');
 
-  // function bet(e) {
-  //   const horseChoice = $(e.target).text();
-  //   $betting.text(horseChoice);
-  // }
-
-  $choice.on('click', (e) => {
+  $horse.on('click', (e) => {
     console.log('hello');
-    const horseChoice = $(e.target).text();
+    const horseChoice = $(e.target).html();
     $betting.text(horseChoice);
   });
+
+  $cashAvailable.html('Your cash: ' + '£' + 250);
+
+  $onePound.on('click', (e)  => {
+    console.log('one');
+    const betAmount = $(e.target).val();
+    console.log(betAmount);
+    $playerWallet.text('Your bet: ' + betAmount);
+  });
+
+  $fivePound.on('click', (e)  => {
+    console.log('five');
+    const betAmount = $(e.target).val();
+    $playerWallet.html('Your bet: ' + betAmount);
+  });
+
+  $tenPound.on('click', (e)  => {
+    console.log('ten');
+    const betAmount = $(e.target).val();
+    $playerWallet.html('Your bet: ' + betAmount);
+  });
+
+  $twentyPound.on('click', (e)  => {
+    console.log('twenty');
+    const betAmount = $(e.target).val();
+    $playerWallet.html('Your bet: ' + betAmount);
+  });
+
+  $fiftyPound.on('click', (e)  => {
+    console.log('fifty');
+    const betAmount = $(e.target).val();
+    $playerWallet.html('Your bet: ' + betAmount);
+  });
+
+  $hundredPound.on('click', (e)  => {
+    console.log('hundred');
+    const betAmount = $(e.target).val();
+    $playerWallet.html('Your bet: ' + betAmount);
+  });
+
+  $reset.on('click', (e) => {
+    console.log('reset');
+    $playerWallet.html('Your bet: ');
+  });
+
+  $placeBet.on('click', (e) => {
+    console.log('bet placed');
+    console.log($cashAvailable - $playerWallet);
+  });
+
+
 
   $( '#go' ).click(function() {
     $('.horse1:first').animate({
@@ -24,6 +82,7 @@ $(() => {
       }
     });
   });
+
   $( '#go' ).click(function() {
     $('.horse2:first').animate({
       left: 1000
@@ -34,6 +93,7 @@ $(() => {
       }
     });
   });
+
   $( '#go' ).click(function() {
     $('.horse3:first').animate({
       left: 1000
@@ -44,6 +104,7 @@ $(() => {
       }
     });
   });
+
   $( '#go' ).click(function() {
     $('.horse4:first').animate({
       left: 1000
@@ -54,6 +115,7 @@ $(() => {
       }
     });
   });
+
   $( '#go' ).click(function() {
     $('.horse5:first').animate({
       left: 1000
@@ -64,6 +126,7 @@ $(() => {
       }
     });
   });
+
   $( '#go' ).click(function() {
     $('.horse6:first').animate({
       left: 1000
@@ -74,6 +137,7 @@ $(() => {
       }
     });
   });
+
 });
 //
 // const $button = $('.choice');
