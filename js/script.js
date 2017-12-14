4024554932600c3a91b823baa4493c47f48f5808
Mark S <markscicluna@hotmail.com>
2017-12-14 18:33:50 +0000

    css fixed

--- js/script.js
+++ js/script.js
@@ -27,6 +27,7 @@ $(() => {
   const $thirdPlace = $('.thirdPlace');
 
   const $cashAvailable = $('.cashAvailable');
+  const $placeBet = $('.placeBet');
   const $potentialReturn = $('.potentialReturn');
 
   let winner = null;
@@ -41,15 +42,15 @@ $(() => {
 
   const $playerWallet = $('.playerWallet');
   const $finalBet = $('.finalBet');
-  const betAmount = 0;
+  let betAmount = 0;
   let wallet = 250;
-  const horseChoice = null;
+  let horseChoice = null;
 
   const horseNames = [null, 'Bullet-Proof', 'Emerald Fire', 'Jalapeno', 'Mischief', 'Please Baby', 'Rise to Glory', 'Tramp Time','Witch Craft'];
 
   //***************************************
   //functions
-  //***************************************
+  //**************************************
 
   function alertresults() {
     $resultDisplay.text('Results are in:' + '1st: ' + winner + '\n2nd: ' + secondPlace + '\n3rd: ' + thirdPlace + '\n4th: ' + fourthPlace + '\n5th: ' + fifthPlace + '\n6th: ' + sixthPlace + '\n7th: ' + seventhPlace + '\n8nd: ' + eighthPlace);
@@ -89,6 +90,82 @@ $(() => {
     $resultpopup.hide();
   });
 
+  $cashAvailable.text(wallet);
+
+  $horse.on('click', (e) => {
+    horseChoice = $(e.target).html();
+    $betting.text(horseChoice);
+    if (horseChoice === '1. Bullet-Proof: 3/1') {
+      $odds.text('3/1');
+    } else if (horseChoice === '2. Emerald Fire: 5/1') {
+      $odds.text('5/1');
+    } else if (horseChoice === '3. Jalapeno: 1/2') {
+      $odds.text('1/2');
+    } else if (horseChoice === '4. Mischief: 7/1') {
+      $odds.text('7/1');
+    } else if (horseChoice === '5. Please Baby: 10/1') {
+      $odds.text('10/1');
+    } else if (horseChoice === '6. Rise to Glory: 4/1') {
+      $odds.text('4/1');
+    } else if (horseChoice === '7. Tramp Time: 20/1') {
+      $odds.text('20/1');
+    } else if (horseChoice === '8. Witch Craft: 9/1') {
+      $odds.text('9/1');
+    }
+  });
+
+  $betButtons.on('click', (e)  => {
+    const amount = $(e.target).val(); // £1
+    const amountWithoutCurrency = amount.replace('£', '');
+    const amountAsInteger = parseInt(amountWithoutCurrency);
+    betAmount = amountAsInteger;
+    $playerWallet.text('£' + betAmount);
+  });
+
+  $placeBet.on('click', function() {
+    const horse = $betting.text();
+    if (horse === 'Bullet-Proof: 3/1') {
+      horseChoice = 'Bullet-Proof';
+      $finalBet.text(horseChoice);
+      $cashAvailable.text(wallet - betAmount);
+      $potentialReturn.text(betAmount * 3);
+    } else if (horse === 'Emerald Fire: 5/1') {
+      horseChoice = 'Emerald Fire';
+      $finalBet.text(horseChoice);
+      $cashAvailable.text(wallet - betAmount);
+      $potentialReturn.html(betAmount * 5);
+    } else if (horse === 'Jalapeno: 1/2') {
+      horseChoice = 'Jalapeno';
+      $finalBet.text(horseChoice);
+      $cashAvailable.text(wallet - betAmount);
+      $potentialReturn.html(betAmount * .5);
+    } else if (horse === 'Mischief: 7/1') {
+      horseChoice = 'Mischief';
+      $finalBet.text(horseChoice);
+      $cashAvailable.text(wallet - betAmount);
+      $potentialReturn.html(betAmount * 7);
+    } else if (horse === 'Please Baby: 10/1') {
+      horseChoice = 'Please Baby';
+      $finalBet.text(horseChoice);
+      $cashAvailable.text(wallet - betAmount);
+      $potentialReturn.html(betAmount * 10);
+    } else if (horse === 'Rise to Glory: 4/1') {
+      horseChoice = 'Rise to Glory';
+      $finalBet.text(horseChoice);
+      $cashAvailable.text(wallet - betAmount);
+      $potentialReturn.html(betAmount * 4);
+    } else if (horse === 'Time Tramp: 20/1') {
+      horseChoice = 'Tramp Time';
+      $finalBet.text(horseChoice);
+      $cashAvailable.text(wallet - betAmount);
+      $potentialReturn.html(betAmount * 20);
+    } else if (horse === 'Witch Craft: 9/1') {
+      horseChoice = 'Witch Craft';
+      $finalBet.text(horseChoice);
+      $cashAvailable.text(wallet - betAmount);
+      $potentialReturn.html(betAmount * 9);
+    }
+  });
 
   function checkWinner() {
     if (horseChoice === winner) {
@@ -181,5 +258,7 @@ $(() => {
       });
     }
   });
+
+
 
 });
